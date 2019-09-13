1c0725ddc7fbd610df5bf4971a4c5ab3b374bbe7
guiide2157 <guide2157@hotmial.com>
2019-09-12 22:13:48 -0700

    add: Restaurant model

---
+++ apps/Restaurant/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+import tagulous.models as tagulous_models
+from apps.Menu.models import Menu
+
+
+class RestaurantTag(tagulous_models.TagTreeModel):
+    ordering = models.PositiveIntegerField(default=0)
+    long_label = models.CharField(blank=True, max_length=255,
+                                  help_text='Set the default title for the tag if not set, use the label')
+    description = models.TextField(blank=True, default='', help_text='Set the default tag description')
+
+    class TagMeta:
+        space_delimiter = False
+
+    @property
+    def title(self):
+        return self.long_label or self.label
+
+    def __str__(self):
+        return self.long_label or self.label
+
+
+class Restaurant(models.Model):
+    detail = models.TextField
+    gallery = models.CharField(max_length=120)
+    id = models.IntegerField
+    image_thumbnail = models.CharField(max_length=120)
+    meta_description = models.CharField(max_length=120)
+    meta_title = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
+    slug = models.CharField(max_length=120)
+    tags = tagulous_models.TagField(blank=True, to=RestaurantTag)
+    featured_menus = models.ManyToManyField(Menu, related_name='restaurant', blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ apps/Restaurant/views.py
@@ -0,0 +1,47 @@
+from .models import Menu
+from .models import Restaurant
+from .serializers import RestaurantSerializer
+from ..Menu.serializers import MenuSerializer
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+
+
+class RestaurantList(generics.ListCreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    lookup_fields = ('id', 'slug')
+
+    def get_featured_menus(self, obj_id):
+        featured_menu_group = Menu.objects.filter(id=obj_id)
+        featured_menus = []
+
+        for menu_group in featured_menu_group:
+            menu_data = MenuSerializer(menu_group).data
+
+            if menu_data is not None:
+                featured_menus.append(menu_data)
+
+        return featured_menus
+
+    def get(self, request, **kwargs):
+        queryset = self.get_queryset()
+        filter_obj = {}
+        keys_map = {'id': 'object_pk', 'slug': 'slug'}
+
+        for field in self.lookup_fields:
+            if self.kwargs.get(field, None) is not None:
+                filter_obj[keys_map[field]] = self.kwargs[field]
+
+        obj = get_object_or_404(queryset, **filter_obj)
+
+        restaurant_serializer = RestaurantSerializer(obj)
+
+        featured_menu_serializer = self.get_featured_menus(obj.object_pk)
+
+        payload = {
+            **restaurant_serializer.data,
+            'featured_menus': featured_menu_serializer
+        }
+
+        return Response(payload, status=200, **kwargs)
